5f4c1a3a934cc6d2e4fb34cc7cb789da33158dd4
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-03-12 15:30:21 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -3,9 +3,19 @@
   var button = document.getElementById("button");
   // Step 2: Define the event listener function
   var onButtonClick = function() {
+    var cipher = document.getElementById("lang").value;
     var name = document.getElementById("name").value;
-    var result = "Your result is "+ name;
-    document.getElementById("message").textContent += result;  
+    var key = document.getElementById("key").value;
+    if (cipher==="cc"){
+
+      
+      var result = "Your cipher is "+ cipher+" and your name is "+name;
+      document.getElementById("message").textContent += result;
+    }
+    else{
+      document.write("This is not Caesar Cipher");
+    }
+      
   };
   // Step 3: Attach event listener to element
   button.addEventListener("click", onButtonClick);
