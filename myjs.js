4bb8eb2200d4adcaf469b802aecbe36aa41c8612
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-03-12 19:54:34 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,21 +2,28 @@
   // Step 1: Find the element we want the event on
   var button = document.getElementById("button");
   // Step 2: Dine the event listener function
-
+  
 
   var onButtonClick = function() {
+
     var cipher = document.getElementById("lang").value;
     var name = document.getElementById("name").value;
     var newname = name;
     var key = document.getElementById("key").value;
+
+     if (newname.length===0){
+      alert("Enter an input! ")
+      //break;
+      }
+    if (key.length==0){
+      alert("Enter a key! ")
+      //break;
+    }
+
     if (cipher==="cc"){
 
    for (i=0;i<newname.length;i++){
      var ascii = newname.charCodeAt(i)-key;
-     //document.write("ascii is "+ascii);
-    if (newname===""){
-    prompt("Enter an input! ");
-    }else{
 
     if (96<ascii && ascii<123){
      var t = String.fromCharCode(newname.charCodeAt(i)-key);
@@ -34,7 +41,7 @@
   }
   }
    }
-    }
+    
     else{
       document.write("This is not Caesar Cipher");
     }
