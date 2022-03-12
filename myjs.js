2ac261bcae41c97cfb9f4382cd4c165952d0bb0d
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-03-12 14:29:07 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,11 @@
-document.write(" --This is javaScript text-- ");
+
+  // Step 1: Find the element we want the event on
+  var button = document.getElementById("button");
+  // Step 2: Define the event listener function
+  var onButtonClick = function() {
+    var name = document.getElementById("name").value;
+    var result = "Your result is "+ name;
+    document.getElementById("message").textContent += result;  
+  };
+  // Step 3: Attach event listener to element
+  button.addEventListener("click", onButtonClick);
